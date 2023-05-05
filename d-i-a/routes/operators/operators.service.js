6f27046a93adc0127979f47e9672817813b87183
amarshaik012 <32787695+amarshaik012@users.noreply.github.com>
2023-05-04 22:08:25 -0500

    Delete d-i-a directory

--- d-i-a/routes/operators/operators.service.js
+++
@@ -1,9 +0,0 @@
-import operatorsStore from './operators.store.json';
-
-export const getOperator = ( operatorId ) => {
-    const operator = operatorsStore.find( 
-        operator => operator.id === operatorId
-    );
-
-    return operator;
-};
